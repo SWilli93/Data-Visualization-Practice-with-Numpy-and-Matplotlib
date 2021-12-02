8939e959ad18c7c0d35c9491fa839d530ea9bd20
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-12-02 12:03:42 -0500

    graph histogram

--- Data Visualization Practice with Numpy and Matplotlib.py
+++ Data Visualization Practice with Numpy and Matplotlib.py
@@ -8,3 +8,16 @@ sunflowers = np.genfromtxt('sunflower_heights.csv',
 # Calculate mean and std of sunflowers here:
 sunflowers_mean = np.mean(sunflowers)
 sunflowers_std = np.std(sunflowers)
+
+# Calculate sunflowers_normal here:
+sunflowers_normal = np.random.normal(sunflowers_mean, sunflowers_std, size=5000)
+
+
+plt.hist(sunflowers,
+         range=(11, 15), histtype='step', linewidth=2,
+        label='observed', normed=True)
+plt.hist(sunflowers_normal,
+        range=(11, 15), histtype='step', linewidth=2,
+       label='normal', normed=True)
+plt.legend()
+plt.show()
