ecd88d8fae7e78a215672cac1ac5eab9eca2968b
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-12-02 12:02:24 -0500

    standard deviation and mean

--- Data Visualization Practice with Numpy and Matplotlib.py
+++ Data Visualization Practice with Numpy and Matplotlib.py
@@ -1,2 +1,10 @@
+import codecademylib
 import numpy as np
 from matplotlib import pyplot as plt
+
+sunflowers = np.genfromtxt('sunflower_heights.csv',
+                           delimiter=',')
+
+# Calculate mean and std of sunflowers here:
+sunflowers_mean = np.mean(sunflowers)
+sunflowers_std = np.std(sunflowers)
